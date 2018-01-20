e7f4d219b6102ec847cb71a355a32a00394b37ac
William <williamribeiromartins@gmail.com>
2018-01-20 20:20:37 -0200

    Alteração no model Criação do banco db.sqlite3

--- coredrca/models.py
+++ coredrca/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from docutils.parsers.rst.directives import flag
 
 class Credito(models.Model):
     d_credito = models.IntegerField()
@@ -6,35 +7,66 @@ class Credito(models.Model):
     a_credito_o = models.IntegerField()
     a_credito_l = models.IntegerField()
     
+    def __str__(self):
+        return self.nome
+
+
 class Departamento(models.Model):
     nome = models.CharField(max_length = 30)
     
+    def __str__(self):
+        return self.nome
+
+
 class Professor(models.Model):
     nome = models.CharField(max_length = 30)
-    departamento = models.ForeignKey(Departamento, null = True)
+    departamento = models.ForeignKey(Departamento, null=False, on_delete=models.CASCADE)
     
+    def __str__(self):
+        return self.nome
+
+
 class Secretaria(models.Model):
     nome = models.CharField(max_length = 30)
     tipo = models.IntegerField() #Tipo '0' para graduados, Tipo '1' para pós-graduados
-    departamento = models.ForeignKey(Departamento, null = True)
+    departamento = models.ForeignKey(Departamento, null=False, on_delete=models.CASCADE)
     
+    def __str__(self):
+        return self.nome
+
+
 class Curso(models.Model):
     nome = models.CharField(max_length = 30)
-    secretaria = models.ForeignKey(Secretaria, null = True)
+    secretaria = models.ForeignKey(Secretaria, null=False, on_delete=models.CASCADE)
     
+    def __str__(self):
+        return self.nome
+    
+
 class Disciplina(models.Model):
     nome = models.CharField(max_length = 30)
     codigo = models.CharField(max_length = 30)
     obr_let = models.CharField(max_length = 30)
     status = models.CharField(max_length = 30)
-    credito = models.ForeignKey(Credito)
+    credito = models.ForeignKey(Credito, null=False, on_delete=models.CASCADE)
     d_requisito = models.ManyToManyField('Disciplina')
-    curso = models.ForeignKey(Curso)
-    professor = models.ForeignKey(Professor, null=True)
+    curso = models.ForeignKey(Curso, null=False, on_delete=models.CASCADE)
+    professor = models.ForeignKey(Professor, null=False, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.nome
+
 
 class Aluno(models.Model):
     nome = models.CharField(max_length = 30)
     matricula = models.IntegerField()
-    curso = models.ForeignKey(Curso)
-    credito = models.ForeignKey(Credito)
-    disciplinas = models.ManyToManyField(Disciplina, null=True)
+    curso = models.ForeignKey(Curso, null=False, on_delete=models.CASCADE)
+    credito = models.ForeignKey(Credito, null=False, on_delete=models.CASCADE)
+    disciplinas = models.ManyToManyField(Disciplina, blank=True)
+    
+    def __str__(self):
+        return self.nome
+    
+    
+    
+    
